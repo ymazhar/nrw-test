2672d7a596813ccc32cb04a2867067d4afce0edb
Yaroslav Mazhar <Yaroslav_Mazhar@epam.com>
2020-12-04 13:50:03 +0200

    add jest config

---
+++ jest.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/serviceWorker.js',
+    '!src/admin/**',
+    '!**/index*.js',
+  ],
+  setupFiles: ['react-app-polyfill/jsdom'],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}',
+  ],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+    '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '<rootDir>/config/jest/fileTransform.js',
+  },
+  transformIgnorePatterns: [
+    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+  modulePaths: [],
+  moduleNameMapper: {
+    '^assets(.*)$': '<rootDir>/src/assets/$1',
+    '^modules(.*)$': '<rootDir>/src/modules/$1',
+    '^pages(.*)$': '<rootDir>/src/pages/$1',
+    '^services(.*)$': '<rootDir>/src/services/$1',
+    '^store(.*)$': '<rootDir>/src/store/$1',
+    '^utils(.*)$': '<rootDir>/src/utils/$1',
+    '^react-native$': 'react-native-web',
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+  },
+  moduleFileExtensions: [
+    'web.js',
+    'js',
+    'web.ts',
+    'ts',
+    'web.tsx',
+    'tsx',
+    'json',
+    'web.jsx',
+    'jsx',
+    'node',
+  ],
+  watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
+};
